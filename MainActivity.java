6e03319150332fa8d4413508e823559d60944461
saleisha57 <saleisha57@yahoo.com>
2017-01-23 19:20:55 -0800

    Add files via upload
    Uploading what I have for the final at this point in time. Will be implementing more classes so that the code looks nicer, and will flow better. Need to do the final program write-up, powerpoint, and design document.

--- MainActivity.java
+++ MainActivity.java
@@ -46,20 +46,72 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void init()
     {
         int number = rand.nextInt(3);
+        int number_2 = rand.nextInt(10);
+
+        System.out.println(number_2);
 
         if(number == 0)
         {
-            for (i = 0; i < 10; i++) {
+            for (i = 0; i < 10; i++)
+            {
                 ff_spicy_choice[i] = get_spicy_fast_food();
-                System.out.println(ff_spicy_choice[i]);
-                //System.out.println(favorite_food_type);
             }//for
             ff = 0;
             for (i = 0; i < 10; i++) {
                 ff_savory_choice[i] = get_savory_fast_food();
-                System.out.println(ff_savory_choice[i]);
-                //System.out.println(favorite_food_type);
             }//for
+
+            for(i = 0; i < 10; i ++)
+                if(number == 0 && number_2 == i && favorite_food_type.equals("Savory"))
+                    System.out.println("Fast Food: " + ff_savory_choice[i]);
+
+            for(i = 0; i < 10; i ++)
+                if(number == 0 && number_2 == i && favorite_food_type.equals("Spicy"))
+                    System.out.println("Fast Food: " + ff_spicy_choice[i]);
+
+            /*
+            if(number == 0 && number_2 == 0 && favorite_food_type.equals("Savory"))
+                System.out.println(ff_savory_choice[0]);
+            if(number == 0 && number_2 == 1 && favorite_food_type.equals("Savory"))
+                System.out.println(ff_savory_choice[1]);
+            if(number == 0 && number_2 == 2 && favorite_food_type.equals("Savory"))
+                System.out.println(ff_savory_choice[2]);
+            if(number == 0 && number_2 == 3 && favorite_food_type.equals("Savory"))
+                System.out.println(ff_savory_choice[3]);
+            if(number == 0 && number_2 == 4 && favorite_food_type.equals("Savory"))
+                System.out.println(ff_savory_choice[4]);
+            if(number == 0 && number_2 == 5 && favorite_food_type.equals("Savory"))
+                System.out.println(ff_savory_choice[5]);
+            if(number == 0 && number_2 == 6 && favorite_food_type.equals("Savory"))
+                System.out.println(ff_savory_choice[6]);
+            if(number == 0 && number_2 == 7 && favorite_food_type.equals("Savory"))
+                System.out.println(ff_savory_choice[7]);
+            if(number == 0 && number_2 == 8 && favorite_food_type.equals("Savory"))
+                System.out.println(ff_savory_choice[8]);
+            if(number == 0 && number_2 == 9 && favorite_food_type.equals("Savory"))
+                System.out.println(ff_savory_choice[9]);
+
+            if(number == 0 && number_2 == 0 && favorite_food_type.equals("Spicy"))
+                System.out.println(ff_spicy_choice[0]);
+            if(number == 0 && number_2 == 1 && favorite_food_type.equals("Spicy"))
+                System.out.println(ff_spicy_choice[1]);
+            if(number == 0 && number_2 == 2 && favorite_food_type.equals("Spicy"))
+                System.out.println(ff_spicy_choice[2]);
+            if(number == 0 && number_2 == 3 && favorite_food_type.equals("Spicy"))
+                System.out.println(ff_spicy_choice[3]);
+            if(number == 0 && number_2 == 4 && favorite_food_type.equals("Spicy"))
+                System.out.println(ff_spicy_choice[4]);
+            if(number == 0 && number_2 == 5 && favorite_food_type.equals("Spicy"))
+                System.out.println(ff_spicy_choice[5]);
+            if(number == 0 && number_2 == 6 && favorite_food_type.equals("Spicy"))
+                System.out.println(ff_spicy_choice[6]);
+            if(number == 0 && number_2 == 7 && favorite_food_type.equals("Spicy"))
+                System.out.println(ff_spicy_choice[7]);
+            if(number == 0 && number_2 == 8 && favorite_food_type.equals("Spicy"))
+                System.out.println(ff_spicy_choice[8]);
+            if(number == 0 && number_2 == 9 && favorite_food_type.equals("Spicy"))
+                System.out.println(ff_spicy_choice[9]);
+                */
             ff = 0;
         }//if
 
@@ -67,15 +119,63 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         {
             for (i = 0; i < 10; i++) {
                 r_spicy_choice[i] = get_spicy_restaurant();
-                System.out.println(r_spicy_choice[i]);
-                //System.out.println(favorite_food_type);
             }//for
             r = 0;
             for (i = 0; i < 10; i++) {
                 r_savory_choice[i] = get_savory_restaurant();
-                System.out.println(r_savory_choice[i]);
-                //System.out.println(favorite_food_type);
             }//for
+
+            for(i = 0; i < 10; i ++)
+                if(number == 1 && number_2 == i && favorite_food_type.equals("Savory"))
+                    System.out.println("Restaurant: " + r_savory_choice[i]);
+
+            for(i = 0; i < 10; i ++)
+                if(number == 1 && number_2 == i && favorite_food_type.equals("Spicy"))
+                    System.out.println("Restaurant: " + r_spicy_choice[i]);
+
+            /*
+            if(number == 1 && number_2 == 0 && favorite_food_type.equals("Savory"))
+                System.out.println(r_savory_choice[0]);
+            if(number == 1 && number_2 == 1 && favorite_food_type.equals("Savory"))
+                System.out.println(r_savory_choice[1]);
+            if(number == 1 && number_2 == 2 && favorite_food_type.equals("Savory"))
+                System.out.println(r_savory_choice[2]);
+            if(number == 1 && number_2 == 3 && favorite_food_type.equals("Savory"))
+                System.out.println(r_savory_choice[3]);
+            if(number == 1 && number_2 == 4 && favorite_food_type.equals("Savory"))
+                System.out.println(r_savory_choice[4]);
+            if(number == 1 && number_2 == 5 && favorite_food_type.equals("Savory"))
+                System.out.println(r_savory_choice[5]);
+            if(number == 1 && number_2 == 6 && favorite_food_type.equals("Savory"))
+                System.out.println(r_savory_choice[6]);
+            if(number == 1 && number_2 == 7 && favorite_food_type.equals("Savory"))
+                System.out.println(r_savory_choice[7]);
+            if(number == 1 && number_2 == 8 && favorite_food_type.equals("Savory"))
+                System.out.println(r_savory_choice[8]);
+            if(number == 1 && number_2 == 9 && favorite_food_type.equals("Savory"))
+                System.out.println(r_savory_choice[9]);
+
+            if(number == 1 && number_2 == 0 && favorite_food_type.equals("Spicy"))
+                System.out.println(r_spicy_choice[0]);
+            if(number == 1 && number_2 == 1 && favorite_food_type.equals("Spicy"))
+                System.out.println(r_spicy_choice[1]);
+            if(number == 1 && number_2 == 2 && favorite_food_type.equals("Spicy"))
+                System.out.println(r_spicy_choice[2]);
+            if(number == 1 && number_2 == 3 && favorite_food_type.equals("Spicy"))
+                System.out.println(r_spicy_choice[3]);
+            if(number == 1 && number_2 == 4 && favorite_food_type.equals("Spicy"))
+                System.out.println(r_spicy_choice[4]);
+            if(number == 1 && number_2 == 5 && favorite_food_type.equals("Spicy"))
+                System.out.println(r_spicy_choice[5]);
+            if(number == 1 && number_2 == 6 && favorite_food_type.equals("Spicy"))
+                System.out.println(r_spicy_choice[6]);
+            if(number == 1 && number_2 == 7 && favorite_food_type.equals("Spicy"))
+                System.out.println(r_spicy_choice[7]);
+            if(number == 1 && number_2 == 8 && favorite_food_type.equals("Spicy"))
+                System.out.println(r_spicy_choice[8]);
+            if(number == 1 && number_2 == 9 && favorite_food_type.equals("Spicy"))
+                System.out.println(r_spicy_choice[9]);
+                */
             r = 0;
 
         }//if
@@ -98,6 +198,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             ff = 0;
             */
         }//if
+
+
 
     }//init
 
@@ -262,17 +364,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             if (r == 7)
             {
                 r++;
-                return "Spicy Soup";
+                return "Spicy Other";
             }
             if (r == 8)
             {
                 r++;
-                return "Spicy Stuff";
+                return "Spicy";
             }
             if (r == 9)
             {
                 r++;
-                return "Spicy Spice";
+                return "Spicy";
             }
         }
 
@@ -322,12 +424,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             if (r == 7)
             {
                 r++;
-                return "";
+                return "BBQ Ribs with Coleslaw";
             }
             if (r == 8)
             {
                 r++;
-                return "";
+                return "Lobster Tail with Melted Butter";
             }
             if (r == 9)
             {
